e9dc0b195e846df3e6e2450543232f1353131867
Luis Felipe Mileo <mileo@kmee.com.br>
2017-10-04 10:46:06 -0300

    [REF] Make org flexible

--- dependencies.sh
+++ dependencies.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
+ORG="kmee"
+
 sudo apt install python3-dev
 virtualenv . --python=python3
 mkdir -p parts
 cd parts
-git clone git@github.com:kmee/ocb.git --branch=11.0 odoo
-git clone git@github.com:kmee/l10n-brazil.git --branch=11.0 l10n-brazil
-git clone git@github.com:kmee/reporting-engine.git --branch=11.0 reporting-engine
+git clone git@github.com:$ORG/ocb.git --branch=11.0 odoo
+git clone git@github.com:$ORG/l10n-brazil.git --branch=11.0 l10n-brazil
+git clone git@github.com:$ORG/reporting-engine.git --branch=11.0 reporting-engine
 #
 cd ..
 #
@@ -19,6 +21,3 @@ source bin/activate
 # Pode ser que vc precise alterar o arquivo com a versão lxml==4.0
 pip install -r parts/odoo/requirements.txt
 pip install -r requirements.txt
-
-
-
