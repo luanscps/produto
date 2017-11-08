c1a8774274329becb65cdb1a7dcff2665a2b1846
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2017-11-08 12:53:40 -0200

    [NEW] Cria o arquivo buildout no script de inicialização

--- init-buildout.sh
+++ init-buildout.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 set -e -x
 virtualenv -p python3 --no-setuptools --no-pip .
-bin/python <(curl https://bootstrap.pypa.io/get-pip.py) --upgrade setuptools==33.1.1 pip zc.buildout
+bin/python <(curl https://bootstrap.pypa.io/get-pip.py) --upgrade setuptools pip zc.buildout
+echo "[buildout]\n\nextends = buildout/base-produto.cfg" >> buildout.cfg
+bin/buildout
 
