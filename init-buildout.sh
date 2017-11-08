3c770e1380958d51a6d3c64d0a16fb706b8c1464
Daniel Sadamo Hirayama <daniel.sadamo@kmee.com.br>
2017-11-08 15:33:34 -0200

    fixup! Corrige a criação de arquivo buildout.cfg

--- init-buildout.sh
+++ init-buildout.sh
@@ -2,6 +2,6 @@
 set -e -x
 virtualenv -p python3 --no-setuptools --no-pip .
 bin/python <(curl https://bootstrap.pypa.io/get-pip.py) --upgrade setuptools pip zc.buildout
-echo "[buildout]\n\nextends = buildout/base-produto.cfg" >> buildout.cfg
+printf "[buildout]\n\nextends = buildout/base-produto.cfg" >> buildout.cfg
 bin/buildout
 
