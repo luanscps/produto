3e82b88222e424e6204934a42f7bdb85962fe590
Luiz Felipe do Divino <lf.divino@gmail.com>
2017-11-10 14:20:42 -0200

    [FIX] Setado versão do setuptools do buildout

--- init-buildout.sh
+++ init-buildout.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e -x
 virtualenv -p python3 --no-setuptools --no-pip .
-bin/python <(curl https://bootstrap.pypa.io/get-pip.py) --upgrade setuptools pip zc.buildout
+bin/python <(curl https://bootstrap.pypa.io/get-pip.py) --upgrade setuptools==36.6.0 pip zc.buildout
 printf "[buildout]\n\nextends = buildout/base-produto.cfg" >> buildout.cfg
 bin/buildout
 
